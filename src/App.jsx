39d5c1289bcca0943754576fe94a6fe35c0623c7
JonathanStaib <JonathanStaibb@gmail.com>
2023-03-22 17:13:53 +0000

    got loading to work

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,16 @@ const App = () => {
 
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const callApi = async (requestParams) => {
-    // mock output
     const data = await axios(requestParams);
-    setData(data);
-    setRequestParams(requestParams);
+    setLoading(true);
+    setTimeout(() => {
+      setData(data);
+      setRequestParams(requestParams);
+      setLoading(false);
+    }, 1000);
   }
 
    
@@ -31,7 +35,7 @@ const App = () => {
         <div>Request Method: {requestParams.method}</div>
         <div>URL: {requestParams.url}</div>
         <Form handleApiCall={callApi} />
-        <Results data={data} />
+        <Results data={data} loading={loading}/>
         <Footer />
       </React.Fragment>
     );
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import './Form.scss';
 
 const Form = (props) => {
-  const [method, useMethod] = useState('GET');
+  const [method, setMethod] = useState('GET');
   const { handleApiCall } = props;
   
   const HandleSubmit = (e) => {
@@ -26,11 +26,14 @@ const Form = (props) => {
             <input name='url' type='text' />
             <button id="go"type="submit">GO!</button>
           </label>
+          <label id="jsonForm">
+            <textarea rows="4" cols="50"> Enter JSON for PUT/POST routes here</textarea>
+          </label>
           <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
+            <span onClick={(e) => setMethod(e.target.innerText)} id="get">GET</span>
+            <span onClick={(e) => setMethod(e.target.innerText)} id="post">POST</span>
+            <span onClick={(e) => setMethod(e.target.innerText)} id="put">PUT</span>
+            <span onClick={(e) => setMethod(e.target.innerText)} id="delete">DELETE</span>
           </label>
         </form>
       </>
